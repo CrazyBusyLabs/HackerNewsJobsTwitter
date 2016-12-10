ddaacfec7604dc3c2c2115ef41a54dcfbd04788e
Jerome Curlier <jcurlier@gmail.com>
2016-12-10 00:01:37 -0800

    add log for starting and getting items

--- listener.js
+++ listener.js
@@ -6,6 +6,8 @@ var twitter = require('twitter');
 var config = {
   databaseURL: "https://hacker-news.firebaseio.com"
 };
+
+console.log('Initializing Firebase');
 firebase.initializeApp(config);
 
 var client = new twitter({
@@ -15,6 +17,7 @@ var client = new twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+console.log('Listening to Firebase');
 var newStoriesRef = firebase.database().ref("/v0/newstories/0"); 
 newStoriesRef.on("value", function(snapshot) {
   var storyRef = firebase.database().ref("/v0/item/"+snapshot.val());
@@ -25,8 +28,8 @@ newStoriesRef.on("value", function(snapshot) {
     var story = storySnapshot.val();
     storyRef.off();
 
+    console.log(`${story.id}: ${story.title} (${story.url})`);
     if (story.type === 'job') {
-      console.log(`${story.id}: ${story.title} (${story.url})`);
       client.post('statuses/update', {status: `${story.title} ${story.url}` },  function(error, tweet, response) {
         if(error) throw error;
           console.log(`@@@ Twitted`);
