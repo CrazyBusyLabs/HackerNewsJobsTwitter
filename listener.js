f74a2f783241a56c8767187ea1986bb3daa1500d
Jerome Curlier <jcurlier@gmail.com>
2018-10-22 16:16:58 -0700

    Update code to use arrows and const

--- listener.js
+++ listener.js
@@ -1,46 +1,75 @@
-'use strict';
-
+// Libraries
 var firebase = require('firebase');
-var twitter = require('twitter');
+var Twitter = require('twitter');
 
+// Configuration
 var config = {
-  databaseURL: "https://hacker-news.firebaseio.com"
+  databaseURL: "https://hacker-news.firebaseio.com",
+  twitter: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  }
 };
 
-console.log('Initializing Firebase');
-firebase.initializeApp(config);
+// twitter client
+let twitter;
 
-var client = new twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+/**
+ * @function
+ * @name myFunction
+ */
+function initialize() {
+  console.log('Initializing Firebase');
+  firebase.initializeApp(config);
 
-console.log('Listening to Firebase');
-var newStoriesRef = firebase.database().ref("/v0/jobstories/0"); 
-newStoriesRef.on("value", function(snapshot) {
-  var storyRef = firebase.database().ref("/v0/item/"+snapshot.val());
-  storyRef.on('value', function(storySnapshot) {
-    if(storySnapshot.val() === null) {
-        return
-    }
-    var story = storySnapshot.val();
-    storyRef.off();
+  twitter = new Twitter(config.twitter);
+}
 
-    console.log(`${story.id}: ${story.title} (${story.url}) [${story.deleted} ${story.dead}]`);
-    if ((typeof story.deleted === 'undefined' || story.deleted === false)
-        && (typeof story.dead === 'undefined' || story.dead === false)) {
-      var message;
-      if (story.url) {
-        message = `${story.title} ${story.url}`;
-      } else {
-        message = `${story.title}`;
+/**
+ * @function
+ * @name listen
+ */
+function listen() {
+  console.log('Listening to Firebase');
+  const newStoriesRef = firebase.database().ref("/v0/jobstories/0");
+
+  // process new events
+  newStoriesRef.on("value", (snapshot) => {
+
+    // get the event data
+    var storyRef = firebase.database().ref(`/v0/item/${snapshot.val()}`);
+    storyRef.on('value', (storySnapshot) => {
+
+      if(storySnapshot.val() === null) {
+          return
       }
-      client.post('statuses/update', { status: message },  function(error, tweet, response) {
-        if(error) throw error;
-          console.log(`${story.id}: @@@ Tweeted`);
-      });
-    }
+      var story = storySnapshot.val();
+      storyRef.off();
+
+      const {id, title, url, deleted, dead} = story;
+      if ((typeof deleted === 'undefined' || deleted === false)
+          && (typeof dead === 'undefined' || dead === false)
+          && url) {
+
+        console.log(`${id}: ${title} (${url}) [${deleted} ${dead}]`);
+        const message = `${title} ${url}`;
+        twitter.post('statuses/update', { status: message },  (error, tweet, response) => {
+          if (error) {
+            console.error(error);
+          } else {
+            console.log(`${id}: @@@ Tweeted`);
+          }
+        });
+      }
+    });
   });
-});
+}
+
+console.log('Initializing the application');
+initialize();
+
+console.log('Start listenting');
+listen();
+console.log('Listening');
